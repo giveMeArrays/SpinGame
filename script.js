aae300e2b976462965c6803ce95fc4c9e8dd1404
the-caped-crusaders-coding-apprentice <70288598+the-caped-crusaders-coding-apprentice@users.noreply.github.com>
2023-12-05 19:44:44 +0200

    added money

--- script.js
+++ script.js
@@ -3,38 +3,45 @@ const controlCon = document.querySelector(".controlsCon")
 const prizeRange = []
 let betAmountPicker = document.getElementById("betAmount")
 let betAmount = betAmountPicker.value
+let multiplier = 0
+let currentDegreeSound = 0
 const Tick = new Audio('Assests/Audio/Tick.wav')
-for (let i = 0; i <= 20; i++) {
+for (let i = 1; i <= 20; i++) {
     prizeRange.push(18 * i)
 }
-
+console.log(prizeRange)
 document.getElementById("spinButton").addEventListener("click", () => {
     betAmount = betAmountPicker.value
-    console.log(betAmount)
     controlCon.style.pointerEvents = "none"
     let currentDegree = prizeRange[Math.floor(Math.random() * prizeRange.length)]
-    if (currentDegree == 0) currentDegree = 360
+    currentDegreeSound = currentDegree
     wheelPrize.style.transition = "none"
     wheelPrize.offsetWidth;
     wheelPrize.style.transform = `rotate(${currentDegree}deg)`
     wheelPrize.style.transition = "transform 3s";
-
+    if(currentDegree == 36 || currentDegree == 180 || currentDegree == 324) multiplier = 1.25
+    if(currentDegree == 72 || currentDegree == 144 || currentDegree == 216 || currentDegree == 288) multiplier = 1.5
+    if(currentDegree == 252 || currentDegree == 108) multiplier = 1.75
+    if(currentDegree == 360) multiplier = 2
     let interval = setInterval(() => {
-        if (currentDegree % 18 != 0 || currentDegree < 0) {
+        if (currentDegreeSound % 18 != 0 || currentDegreeSound < 0) {
             clearInterval(interval)
         }
         else {
             Tick.play()
-            currentDegree -= 18
+            currentDegreeSound -= 18
         }
     }, 300)
 
     setTimeout(() => {
-        alert("winner")
+        console.log(currentDegree)
+        if (multiplier != 0) alert(`winner; You win ${betAmount * multiplier}`)
+        else alert("loser")
         wheelPrize.style.transition = "transform 2s ease-out"
         wheelPrize.style.transform = "rotate(0deg)"
         controlCon.style.pointerEvents = "all"
         clearInterval(interval)
+        multiplier = 0
     }, 3000)
 })
 
